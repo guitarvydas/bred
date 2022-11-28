a8d4e6bb69fecfb0120d8a840f16f17e87651f1a
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-28 07:52:33 -0500

    breddir is parameter $3

--- bred.bash
+++ bred.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
-## usage: ./bred.bash message.bred container.u0d >container.0d
+## usage: ./bred.bash message.bred container.u0d breddir >container.0d
 pattern=pattern_${RANDOM}
-bdir=../bred
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab  --support=support.js <$1 >${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm2.fab  --support=support.js <$1 >>${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm3.fab  --support=support.js <$1 >>${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredfab.fab --support=${bdir}/support.js <$1 > ${pattern}.fab
+bdir=$3
+support=--support=${bdir}/support.js
+./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.ohm
+./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm2.fab ${support} <$1 >>${pattern}.ohm
+./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm3.fab  ${support} <$1 >>${pattern}.ohm
+./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredfab.fab ${support} <$1 > ${pattern}.fab
 ./fab/fab - NestingGrammar ${pattern}.ohm ${pattern}.fab <$2
 rm -f ${pattern}.ohm ${pattern}.fab
 
