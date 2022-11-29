fc65a1ae5ee57cb0cafd388ba0706576ce7d53cd
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-29 17:43:37 -0500

    change bred.bash to use bred1.bash and bred2.bash

--- bred.bash
+++ bred.bash
@@ -3,10 +3,9 @@
 pattern=pattern_${RANDOM}
 bdir=$3
 support=--support=${bdir}/support.js
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm2.fab ${support} <$1 >>${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm3.fab  ${support} <$1 >>${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredfab.fab ${support} <$1 > ${pattern}.fab
+./bred1.bash - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.ohm
+./bred2.bash - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.fab
+
 ./fab/fab - NestingGrammar ${pattern}.ohm ${pattern}.fab <$2
 rm -f ${pattern}.ohm ${pattern}.fab
 
