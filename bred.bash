67349b39622ccf42dedf99e052744dee4787e72d
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-29 17:47:00 -0500

    use ${bdir} in bred.bash

--- bred.bash
+++ bred.bash
@@ -3,8 +3,8 @@
 pattern=pattern_${RANDOM}
 bdir=$3
 support=--support=${bdir}/support.js
-./bred1.bash - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.ohm
-./bred2.bash - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.fab
+${bdir}/bred1.bash - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.ohm
+${bdir}/bred2.bash - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.fab
 
 ./fab/fab - NestingGrammar ${pattern}.ohm ${pattern}.fab <$2
 rm -f ${pattern}.ohm ${pattern}.fab
