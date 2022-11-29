28c5e5aa50c250dfd2aaca08ddf89f296ecb2305
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-29 17:33:08 -0500

    bred1.bash

--- bred1.bash
+++ bred1.bash
@@ -1,12 +1,9 @@
 #!/bin/bash
-## usage: ./bred.bash message.bred container.u0d breddir >container.0d
+## usage: ./bred1.bash message.bred container.u0d breddir
 pattern=pattern_${RANDOM}
 bdir=$3
 support=--support=${bdir}/support.js
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm2.fab ${support} <$1 >>${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm3.fab  ${support} <$1 >>${pattern}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredfab.fab ${support} <$1 > ${pattern}.fab
-./fab/fab - NestingGrammar ${pattern}.ohm ${pattern}.fab <$2
-rm -f ${pattern}.ohm ${pattern}.fab
+./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1
+./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm2.fab ${support} <$1
+./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm3.fab  ${support} <$1
 
