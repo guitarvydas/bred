420b5ede1679ed936e1b860f8467f40043ddb70c
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-29 17:58:33 -0500

    bred.bash passes $1,$2,$3 through to bred1.bash and bred2.bash

--- bred.bash
+++ bred.bash
@@ -3,8 +3,8 @@
 pattern=pattern_${RANDOM}
 bdir=$3
 support=--support=${bdir}/support.js
-${bdir}/bred1.bash - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.ohm
-${bdir}/bred2.bash - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab ${support} <$1 >${pattern}.fab
+${bdir}/bred1.bash  $1 $2 $3 >${pattern}.ohm
+${bdir}/bred2.bash  $1 $2 $3 >${pattern}.ohm
 
 ./fab/fab - NestingGrammar ${pattern}.ohm ${pattern}.fab <$2
 rm -f ${pattern}.ohm ${pattern}.fab
