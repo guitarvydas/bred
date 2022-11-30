0d2c480c71f70b5644b299de16f871f58d800c23
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-30 08:10:21 -0500

    keep temp files during bootstrap

--- bred.bash
+++ bred.bash
@@ -1,11 +1,19 @@
 #!/bin/bash
 ## usage: ./bred.bash message.bred container.u0d breddir >container.0d
-pattern=pattern_${RANDOM}
+
+# when released...
+#pattern=pattern_${RANDOM}
+
+# for now, during bootstrap
+pattern=pattern
+
 bdir=$3
 support=--support=${bdir}/support.js
 ${bdir}/bred1.bash  $1 $2 $3 >${pattern}.ohm
 ${bdir}/bred2.bash  $1 $2 $3 >${pattern}.fab
 
 ./fab/fab - NestingGrammar ${pattern}.ohm ${pattern}.fab <$2
-rm -f ${pattern}.ohm ${pattern}.fab
+
+# when released...
+#rm -f ${pattern}.ohm ${pattern}.fab
 
