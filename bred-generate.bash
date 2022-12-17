85a4c45139bcb7dd8e2047c4fe23db573121042c
Paul Tarvydas <guitarvydas@gmail.com>
2022-12-16 23:17:46 -0500

    qualify fab

--- bred-generate.bash
+++ bred-generate.bash
@@ -8,9 +8,9 @@ bdir=$3
 support=--support=${bdir}/support.js
 
 # generate .ohm file
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab           <${spec}  >${fname}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm2.fab           <${spec} >>${fname}.ohm
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm3.fab           <${spec} >>${fname}.ohm
+${bdir}/fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm1.fab           <${spec}  >${fname}.ohm
+${bdir}/fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm2.fab           <${spec} >>${fname}.ohm
+${bdir}/fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredohm3.fab           <${spec} >>${fname}.ohm
 # generate .fab file
-./fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredfab.fab ${support} <${spec} >${fname}.fab
+${bdir}/fab/fab - Bred ${bdir}/bred.ohm ${bdir}/bredfab.fab ${support} <${spec} >${fname}.fab
 
